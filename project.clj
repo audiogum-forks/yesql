f49720ca4649e691b80306b00ff25aff5f6067d7
Joe Littlejohn <joelittlejohn@gmail.com>
2017-12-14 22:47:38 +0000

    Version 0.5.3-1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yesql "0.5.3"
+(defproject audiogum/yesql "0.5.3-1"
   :description "A Clojure library for using SQL"
   :url "https://github.com/krisajenkins/yesql"
   :license {:name "Eclipse Public License"
